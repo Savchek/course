bc88c40bc46b42f3c0b704c6a354288b5e6fc30e
Savchek <danil.753253@gmail.com>
2020-04-28 22:35:25 +0300

    Updates

---
+++ precache-manifest.412cf368790b8dc2bf2d97eb3a7a4e97.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25d5d1f9b4a94f3e7795ee809850ea7a",
+    "url": "/course/index.html"
+  },
+  {
+    "revision": "4f8a6838eb5e3808955b",
+    "url": "/course/static/css/main.8b4e100e.chunk.css"
+  },
+  {
+    "revision": "8e883a6fb9ecbadc43da",
+    "url": "/course/static/js/2.fff0c860.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/course/static/js/2.fff0c860.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f8a6838eb5e3808955b",
+    "url": "/course/static/js/main.439e2ee2.chunk.js"
+  },
+  {
+    "revision": "8d3f71ae02bed9778e1c",
+    "url": "/course/static/js/runtime-main.2817b438.js"
+  },
+  {
+    "revision": "7d1873ed2caba1e5b0a31d4d8bb4d97c",
+    "url": "/course/static/media/cross.7d1873ed.svg"
+  },
+  {
+    "revision": "75a90539114ccd91ca722ebfdff29941",
+    "url": "/course/static/media/cycle.75a90539.svg"
+  }
+]);
